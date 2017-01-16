f99552aca47a2c22f83c9d5762e1f7325967f6a5
Gab <wei910726@163.com>
2017-01-16 21:35:44 +0800

    add event_manage function

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from poll.models import Event
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -8,7 +11,21 @@ def login_action(request):
     if request.method == "POST":
        username = request.POST.get('username','')
        password = request.POST.get('password','')
-       if username == 'gab' and password == 'gab1234':
-          return render(request,'event.html')
+       user = auth.authenticate(username=username,password=password)
+       if user is not None:
+          auth.login(request, user)
+          response = HttpResponseRedirect('/event_manage/')
+          #response.set_cookie('user',username,3600)
+          request.session['user'] = username
+          return response
        else:
           return render(request,'index.html',{'error':'username or password error!'})
+
+@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    username = request.session.get('user','')
+    return render(request,'event.html',{'user':username,'events':event_list})
+@login_required
+def logout(request):
+    return render(request,'index.html',{'logout':'退出成功！'})
--- polls/settings.py
+++ polls/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'poll',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -113,7 +114,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
