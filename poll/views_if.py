25a951e5c95047412eae485edecdbdcda8b5ac67
Gab <wei910726@163.com>
2017-02-13 21:13:29 +0800

    添加发布会查询接口

--- poll/views_if.py
+++ poll/views_if.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from poll.models import Event,Guest
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 import time
 
 
@@ -79,5 +79,34 @@ def get_event_list(request):
     name = request.POST.get('name', '')
 
     if eid == "" or name == "":
-        return JsonResponse({})
+        return JsonResponse({'status': 10021, 'message': 'parameter miss'})
 
+    if eid != '':
+        event = {}
+        try:
+            result = Event.objects.get(id=eid)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 10022, 'message': 'query result is empty'})
+        else:
+            event['name'] = result.name
+            event['limit'] = result.limit
+            event['status'] = result.status
+            event['address'] = result.address
+            event['start_time'] = result.start_time
+            return JsonResponse({'status': 200, 'message':'success', 'data': event})
+
+    if name != '':
+        datas = []
+        results = Event.objects.filter(name__contains=name)
+        if results:
+            for r in results:
+                 event = {}
+                 event['name'] = r.name
+                 event['limit'] = r.limit
+                 event['status'] = r.status
+                 event['address'] = r.address
+                 event['start_time'] = r.start_time
+                 datas.append(event)
+            return JsonResponse({'status': 200, 'message':'success', 'data': datas})
+        else:
+            return JsonResponse({'status': 10022, 'message': 'query result is empty'})
